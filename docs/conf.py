f11982bba42007c2c3a8e63afa3e319831501c04
aerte <62424399+aerte@users.noreply.github.com>
2024-02-18 18:08:35 +0100

    Doc

--- docs/conf.py
+++ docs/conf.py
@@ -36,8 +36,8 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
+# List of patterns, relative to docs directory, that match files and
+# directories to ignore when looking for docs files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
